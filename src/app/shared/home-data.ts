f4b6372133eb5aa2156316f06781712f07c7c316
Palak Shah <palak.shah@zeuslearning.mumbai>
2023-06-08 14:46:51 +0530

    with dist

---
+++ src/app/shared/home-data.ts
@@ -0,0 +1,10 @@
+export class HomeData {
+  partSize: number;
+  gsSize: number;
+  fillAnswer: string;
+  accessible: boolean;
+  split: boolean;
+  interchangefn: boolean;
+  isqrt: boolean;
+  numlist: boolean;
+}
